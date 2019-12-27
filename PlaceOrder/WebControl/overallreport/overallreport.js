61695ca2f4d3479b8fecd19b855cb08d3d82b613
obwan <azurazazel@gmail.com>
2019-12-27 09:42:45 +0800

    alert changes

--- PlaceOrder/WebControl/overallreport/overallreport.js
+++ PlaceOrder/WebControl/overallreport/overallreport.js
@@ -1,8 +1,5 @@
 /* by zhuhy */
 
-// 模拟页面查询数据
-var source = '{{"散装测试","散装测试客户","600.00000000","38.65000000","600.00000000","072201","2019-07-24 10:26:57","2019-07-24 10:27:08","2019-07-22 16:08:26","XJY2019072200006618"},{"散装测试1","散装测试客户","601.00000000","39.65000000","601.00000000","072202","2019-07-25 10:26:57","2019-07-26 10:27:08","2019-07-23 16:08:26","XJY2019072200006619"}}';
-
 // function formatJson(ary) {
 //     var result = [];
 //     var keys = ary[0];
@@ -42,14 +39,12 @@ window.onload = function () {
 function loadData() {
     var result = $ctx.get("result");
 
-    alert("result: " + JSON.stringify(result));
-
     if (result) {
         result = JSON.parse(result);
         var datas = result.datas;
         if (datas) {
             var tranformedData = datas.replace(/\{/g, "[").replace(/\}/g, "]");
-            // alert("转换后数据： " + tranformedData);
+            alert("转换后数据： " + tranformedData);
             $("#reportTable").html(get_contain(tranformedData));
         } else {
             alert("获取数据出错！");
